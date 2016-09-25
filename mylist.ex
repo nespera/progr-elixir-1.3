94cdde89da862fe80e5f3983ffb9c8e3ccea6591
chrisr <c@24.org.uk>
2016-09-25 13:28:07 +0100

    Added caesar function

--- mylist.ex
+++ mylist.ex
@@ -9,5 +9,10 @@ defmodule MyList do
   def max(list), do: _max(list, 0)
   defp _max([], maxval), do: maxval
   defp _max([head | tail], maxval) when head > maxval, do: _max(tail, head)
-  defp _max([head | tail], maxval), do: _max(tail, maxval)
+  defp _max([_head | tail], maxval), do: _max(tail, maxval)
+
+  def caesar([], _n), do: []
+  def caesar([head | tail], n), do: [_caesar(head + n) | caesar(tail, n)]
+  defp _caesar(char) when char <= ?z, do: char
+  defp _caesar(char), do: _caesar(char - 26)
 end
