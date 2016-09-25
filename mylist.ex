718b088373ca7f5cf68034b6d8b60588ba2b4def
chrisr <c@24.org.uk>
2016-09-25 13:18:56 +0100

    Created recursive max function

--- mylist.ex
+++ mylist.ex
@@ -4,4 +4,10 @@ defmodule MyList do
 
   def mapsum([], _), do: 0
   def mapsum([head | tail], f), do: f.(head) + mapsum(tail, f)
+
+  def max([]), do: raise "Cannot find max of empty list"
+  def max(list), do: _max(list, 0)
+  defp _max([], maxval), do: maxval
+  defp _max([head | tail], maxval) when head > maxval, do: _max(tail, head)
+  defp _max([head | tail], maxval), do: _max(tail, maxval)
 end
