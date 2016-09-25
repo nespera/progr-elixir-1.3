210bb3b9d499ec7f576b9615805a5d5d117e6bea
chrisr <c@24.org.uk>
2016-09-25 13:09:25 +0100

    Created the mapsum function

--- mylist.ex
+++ mylist.ex
@@ -1,4 +1,7 @@
 defmodule MyList do
   def sum([]), do: 0
   def sum([ head | tail ]), do: head + sum(tail)
+
+  def mapsum([], _), do: 0
+  def mapsum([head | tail], f), do: f.(head) + mapsum(tail, f)
 end
