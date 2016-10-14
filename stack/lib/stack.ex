be3dcbd71ca8fa3f8eb75325d1320934221a7c46
chrisr <c@24.org.uk>
2016-10-14 20:58:35 +0100

    Added the supervisor to my stack

--- stack/lib/stack.ex
+++ stack/lib/stack.ex
@@ -1,2 +1,19 @@
 defmodule Stack do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      worker(Stack.Server, [[3, 4, 5, 12, 13]]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Stack.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
